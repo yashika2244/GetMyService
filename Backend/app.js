d96ccf2d1c3e7de4376fcae28744aa7e2f83c01b
Yashu Rajput <yashu@gmail.com>
2025-06-21 07:34:47 +0530

    7:30

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://service.vercel.app',
+    'https://serivce.vercel.app',
   ],
   credentials: true,
 }));
